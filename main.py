2702eac5021590f4635c62d035738395a28c5388
Raquelle M. Campos <raquellecampos12@gmail.com>
2024-03-23 22:46:03 -0400

    alterações

--- main.py
+++ main.py
@@ -2,14 +2,19 @@ from tkinter import *
 from tkinter import ttk
 
 # cores
-cor1 = '1e1f1e' # preto
+cor1 = '#3b3b3b' # preto
+cor2 = '#feffff' # branco
+cor3 = '#38576b' # azul
+cor4 = 'ECEFF1' # cinza
+cor5 = 'FFAB40' # laranja
 
 janela = Tk()
 janela.title('Calculadora')
 # definir largura e comprimento
 janela.geometry('235x318') 
+janela.config(bg=cor1)
 
-frame_tela = Frame(janela, width=235, height=50, bg=cor1)
+frame_tela = Frame(janela, width=235, height=50)
 frame_tela.grid(row=0, column=0)
 
 janela.mainloop()
