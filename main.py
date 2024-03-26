8618253d6a191d49e54bd81fb3d2e31ea2d83967
Raquelle M. Campos <raquellecampos12@gmail.com>
2024-03-25 22:35:34 -0400

    definindo função de inserir valores

--- main.py
+++ main.py
@@ -21,14 +21,29 @@ frame_tela.grid(row=0, column=0)
 frame_corpo = Frame(janela, width=235, height=268)
 frame_corpo.grid(row=1, column=0)
 
+# criando a função com a Lógica
+# eval permite realizar expressões matamáticas dentro dela
+todos_valores = ''
+def inserir_valores(evento):
+    global todos_valores
+    todos_valores = evento
+    todos_valores = todos_valores + str(evento) # concatenando valores passados na variável
+
+    resultado = eval('9/9')
+    # passando valor para tela
+    valor_texto.set(todos_valores)
+
+valor_texto = StringVar()
+
+
 # Criando Labels
-app_label = Label(frame_tela, text='123456789', width=16, height=2, padx=7, relief=FLAT, anchor='e', justify=RIGHT, font=('Ivy 18'),bg=cor3, fg=cor2)
+app_label = Label(frame_tela, textvariable=valor_texto, width=16, height=2, padx=7, relief=FLAT, anchor='e', justify=RIGHT, font=('Ivy 18'),bg=cor3, fg=cor2)
 app_label.place(x=0, y=0)
 
 # Criando os botões
 btn1 = Button(frame_corpo, text='C', width=11, height=2, bg=cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
 btn1.place(x=0, y=0) # x verical y horizontal
-btn2 = Button(frame_corpo, text='%', width=5, height=2, bg=cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
+btn2 = Button(frame_corpo, command=lambda: inserir_valores('%'), text='%', width=5, height=2, bg=cor4, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
 btn2.place(x=118, y=0)
 btn3 = Button(frame_corpo, text='/', width=5, height=2, bg=cor5, fg=cor2, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
 btn3.place(x=177, y=0)
@@ -66,5 +81,6 @@ btn17 = Button(frame_corpo, text='.', width=5, height=2, bg=cor4, font=('Ivy 13 
 btn17.place(x=118, y=208)
 btn18 = Button(frame_corpo, text='=', width=5, height=2, bg=cor5, fg=cor2, font=('Ivy 13 bold'), relief=RAISED, overrelief=RIDGE)
 btn18.place(x=177, y=208)
+
 
 janela.mainloop()
