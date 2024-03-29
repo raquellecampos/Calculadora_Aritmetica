1e11d141df05de75f55130d307105cf148f0cb16
Raquelle M. Campos <raquellecampos12@gmail.com>
2024-03-28 23:16:49 -0400

    calculadora com Python e Tkinter

---
+++ Segunda_Calculadora/calculadora_funcoes.py
@@ -0,0 +1,6 @@
+import tkinter as tk
+
+def janela() -> tk.Tk:
+    root = tk.Tk()
+    root.title('Calculadora')
+    return root
---
+++ Segunda_Calculadora/main.py
@@ -0,0 +1,8 @@
+from calculadora_funcoes import janela
+
+def main():
+    root = janela()
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
